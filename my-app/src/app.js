1d230dfc89190ec7fabbe4bcf79e65c8c24a3d3c
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-07-24 11:39:19 +0300

    first commit

---
+++ my-app/src/app.js
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+
+export const App = () => {
+	return (
+		<div className={styles.app}>
+			<div>
+				<h1>Hello!</h1>
+			</div>
+		</div>
+	);
+};
+
+export default App;
