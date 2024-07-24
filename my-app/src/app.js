15a6e4e4aa33c8cf8bcf29f1253c346cc510aa9b
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-07-24 17:31:04 +0300

    add components

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,13 +1,22 @@
+import { Header, UserBlock } from './components';
 import styles from './app.module.css';
 
+const getUserFromServer = () => ({
+	id: 'a1100',
+	name: 'Иван',
+	age: 23,
+	email: 'Ivan@mail.ru',
+	phone: '+7-999-99-99',
+});
+
 export const App = () => {
+	const { name, age, emeil, phone } = getUserFromServer();
+
 	return (
 		<div className={styles.app}>
-			<div>
-				<h1>Hello!</h1>
-			</div>
+			<Header currentUser={name} />
+			<hr />
+			<UserBlock name={name} age={age} email={emeil} phone={phone} />
 		</div>
 	);
 };
-
-export default App;
